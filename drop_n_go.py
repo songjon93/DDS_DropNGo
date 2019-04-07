449fdb0a9f776a214e8a18fd968680b41207e4a0
SPARK <https://github.com/songjon93>
2019-04-07 04:22:06 -0400

    syntax fix

--- drop_n_go.py
+++ drop_n_go.py
@@ -6,5 +6,8 @@ from time import sleep
 while True:
     uid = rfid_module.scan()
     # DB.update(DB.col, uid, 1)
-    print(DB.query(DB.col, uid))
+    if (DB.query(DB.col, uid)):
+        DB.new(DB.col, uid)
+        DB.update(DB.col, uid, 1)
+
     sleep(5)
