539503f196a0c8f0cb10c430de6da93e9690eb9c
SPARK <https://github.com/songjon93>
2019-04-07 09:27:07 -0400

    motor fix

--- output_module.py
+++ output_module.py
@@ -96,8 +96,9 @@ def clean_up():
 	GPIO.clean_up()
 
 motor.start(0)
-set_angle(0, 180, 1)
-set_angle(180, 0, -1)
+# set_angle(0, 180, 1)
+# set_angle(180, 0, -1)
+motor.ChangeDutyCycle(12)
 # set_angle(90)
 # set_angle(0)
 # set_angle(0)
