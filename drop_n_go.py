3fdccbbb6390f430610a863c87f36b15b4ce525f
SPARK <https://github.com/songjon93>
2019-04-07 04:59:22 -0400

    watson integrated

--- drop_n_go.py
+++ drop_n_go.py
@@ -1,18 +1,25 @@
 import DB
 import watson_helper
 import rfid_module
-import led_module
+import output_module
 from time import sleep
 
 while True:
     led_module.led_on()
     uid = rfid_module.scan()
     led_module.led_off()
-    
+
+    print("Inspecting To Go Box.\n")
+    classifier_res = watson_helper.ask_watson_list("./images/f_test_1.JPG")
+
+    if (classifier_res[0][1] < 0):
+        print("This is not a certified To Go Box.\n")
+        continue
+
     # DB.update(DB.col, uid, 1)
     if (DB.query(DB.col, uid) < 0):
         DB.new(DB.col, uid)
 
-    print("You have {} boxes remaining".format(DB.update(DB.col, uid, 1)))
+    print("You have {} boxes remaining.\n".format(DB.update(DB.col, uid, 1)))
 
     sleep(5)
--- output_module.py
+++ output_module.py
@@ -6,8 +6,10 @@ GPIO.setwarnings(False)
 
 ledPin = 27
 push_button = 22
+buzzer = 23
 
 GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(buzzer, GPIO.OUT)
 GPIO.setup(push_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
