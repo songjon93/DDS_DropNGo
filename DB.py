efd1b313bafa799b2b408e42affa6a7bd1708c51
SPARK <https://github.com/songjon93>
2019-04-07 03:59:19 -0400

    rfid + db integrated

--- DB.py
+++ DB.py
@@ -3,11 +3,11 @@ import dns
 import sys
 
 
-def query(collection, user_id):
-    return(collection.find_one({"_id": user_id})["num_box"])
+def query(collection, uid):
+    return(collection.find_one({"_id": uid})["num_box"])
 
-def update(collection, user_id, inc_val):
-    collection.update_one({"_id" : user_id}, {"$inc": {"num_box" : inc_val}})
+def update(collection, uid, inc_val):
+    collection.update_one({"_id" : uid}, {"$inc": {"num_box" : inc_val}})
 
 
 
@@ -15,21 +15,21 @@ client = pymongo.MongoClient("mongodb+srv://spark9312:hackathon_SungJun@dropngod
 db = client.user_db
 col = db.user
 
-# Add new User
-# collection.insert_one({"_id" : "F0007Q1", "num_box" : 0})
-if (len(sys.argv) == 3):
-    # Box returned
-    if (sys.argv[1] == "return"):
-        update(col, sys.argv[2], 1)
-        print("You have {} boxes remaining".format(query(col, sys.argv[2])))
-    # Box used
-    if (sys.argv[1] == "use"):
-        update(col, sys.argv[2], -1)
-        print("You have {} boxes remaining".format(query(col, sys.argv[2])))
-
-    # Query how man box a user has
-    if (sys.argv[1] == "query"):
-        print("You have {} boxes remaining".format(query(col, sys.argv[2])))
-
-else:
-    print("This program requires two parameters: <Mode: return, use, query> <User ID>")
+# # Add new User
+# # collection.insert_one({"_id" : "F0007Q1", "num_box" : 0})
+# if (len(sys.argv) == 3):
+#     # Box returned
+#     if (sys.argv[1] == "return"):
+#         update(col, sys.argv[2], 1)
+#         print("You have {} boxes remaining".format(query(col, sys.argv[2])))
+#     # Box used
+#     if (sys.argv[1] == "use"):
+#         update(col, sys.argv[2], -1)
+#         print("You have {} boxes remaining".format(query(col, sys.argv[2])))
+#
+#     # Query how man box a user has
+#     if (sys.argv[1] == "query"):
+#         print("You have {} boxes remaining".format(query(col, sys.argv[2])))
+#
+# else:
+#     print("This program requires two parameters: <Mode: return, use, query> <User ID>")
--- drop_n_go.py
+++ drop_n_go.py
@@ -1,6 +1,9 @@
 import DB
 import watson_helper
 import rfid_module
+from time import sleep
 
 while True:
-    rfid_module.scan()
+    uid = rfid_module.scan()
+    DB.update(DB.col, uid, 1)
+    sleep(5)
