38a20c11e950afd23cd55bae5cf21038ec55add2
SPARK <https://github.com/songjon93>
2019-04-07 07:33:35 -0400

    new chime

--- drop_n_go.py
+++ drop_n_go.py
@@ -18,16 +18,17 @@ while True:
     print(classifier_res)
     if (classifier_res[0][1] < 0):
         print("This is not a certified To Go Box.\n")
+        output_module.buzzer_wrong()
         continue
 
+    # output_module.buzzer_on();
+    output_module.buzzer_correct()
     # DB.update(DB.col, uid, 1)
     if (DB.query(DB.col, uid) < 0):
         DB.new(DB.col, uid)
 
-    # output_module.buzzer_on();
-    output_module.run()
     # playSong(starwars_notes, starwars_beats, 0.2)
     sleep(.5)
     print("You have {} boxes remaining.\n".format(DB.update(DB.col, uid, 1)))
-    output_module.buzzer_off();
+    # output_module.buzzer_off();
     sleep(3)
