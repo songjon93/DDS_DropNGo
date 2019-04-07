f8a2b4623acf78772040cae8cf3dc9acba67f56e
SPARK <https://github.com/songjon93>
2019-04-07 07:53:48 -0400

    adjust length

--- output_module.py
+++ output_module.py
@@ -4,6 +4,7 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+servo = 4
 ledPin = 27
 push_button = 22
 buzzer = 18
@@ -29,8 +30,27 @@ TONES = {"c6":1047,
 	"d4":294,
 	"c4":262}
 
-CORRECT =	[["e5", 8], ["g5", 4]]
+CORRECT =	[["a5", 32], ["c5", 16]]
 WRONG = [["ab4", 3]]
+
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(buzzer, GPIO.OUT)
+GPIO.setup(push_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(servo, GPIO.OUT)
+
+
+# GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
+def led_on():
+	GPIO.output(ledPin, GPIO.HIGH)
+
+def led_off():
+	GPIO.output(ledPin, GPIO.LOW)
+
+def buzzer_on():
+    GPIO.output(buzzer, 1)
+
+def buzzer_off():
+    GPIO.output(buzzer, 0)
 
 def buzzer_correct():
 	p = GPIO.PWM(buzzer, 440)
@@ -56,55 +76,6 @@ def playTone(p, tone):
 		p.start(0.5)
 		time.sleep(duration)
 		p.stop()
-
-
-GPIO.setup(ledPin, GPIO.OUT)
-GPIO.setup(buzzer, GPIO.OUT)
-GPIO.setup(push_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-
-# GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
-def led_on():
-	GPIO.output(ledPin, GPIO.HIGH)
-
-def led_off():
-	GPIO.output(ledPin, GPIO.LOW)
-
-def buzzer_on():
-    GPIO.output(buzzer, 1)
-
-def buzzer_off():
-    GPIO.output(buzzer, 0)
-
-def playScale(scale, pause):
-    '''
-    scale: scale name to be played
-    pause: pause between each notes played
-
-    This function plays the given scale in every available octave
-    I used this to test what was audible on the buzzer
-    '''
-    for i in range(0, 5):
-        for note in scale:
-            tone1.ChangeFrequency(note[i])
-            time.sleep(pause)
-    tone1.stop()
-
-def playSong(songnotes, songbeats, tempo):
-    '''
-    songnotes: list of the melodies notes
-    songbeats: list of melodies beat times
-    tempo: speed of song, this is not traditional tempo in bpm like on a metronome,
-        but more like a multiplier for whatever the notes are so a tempo value of 2
-        make it play twice as fast. Adjust this by ear.
-
-    This function plays the melody, simply by iterating through the list.
-    '''
-    tone1.ChangeDutyCycle(50)
-    for i in range(0, len(songnotes)):
-        tone1.ChangeFrequency(songnotes[i])
-        time.sleep(songbeats[i]*tempo)
-    tone1.ChangeDutyCycle(0)
 
 def clean_up():
 	GPIO.clean_up()
