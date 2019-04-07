960dd0b3b218b2249dc3b6dce159e7ffe6eb55cc
SPARK <https://github.com/songjon93>
2019-04-07 07:18:06 -0400

    buzzer freq

--- drop_n_go.py
+++ drop_n_go.py
@@ -24,9 +24,9 @@ while True:
     if (DB.query(DB.col, uid) < 0):
         DB.new(DB.col, uid)
 
-    # output_module.buzzer_on();
-    playSong(starwars_notes, starwars_beats, 0.2)
-    sleep(2)
+    output_module.buzzer_on();
+    # playSong(starwars_notes, starwars_beats, 0.2)
+    sleep(.5)
     print("You have {} boxes remaining.\n".format(DB.update(DB.col, uid, 1)))
     output_module.buzzer_off();
     sleep(3)
