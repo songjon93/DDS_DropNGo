18d78dcaf1c58f4008245aae2709f87ee9ff54d5
SPARK <https://github.com/songjon93>
2019-04-07 04:17:58 -0400

    db uid change

--- DB.py
+++ DB.py
@@ -4,13 +4,13 @@ import sys
 
 
 def query(collection, uid):
-    return(collection.find_one({"_id": uid})["num_box"])
+    return(collection.find_one({"uid": uid})["num_box"])
 
 def update(collection, uid, inc_val):
-    collection.update_one({"_id" : uid}, {"$inc": {"num_box" : inc_val}})
+    collection.update_one({"uid" : uid}, {"$inc": {"num_box" : inc_val}})
     return query(collection, uid)
 def new(collection, uid):
-    collection.insert_one({"_id" : uid, "num_box" : 0})
+    collection.insert_one({"uid" : uid, "num_box" : 0})
 
 
 client = pymongo.MongoClient("mongodb+srv://spark9312:hackathon_SungJun@hackathon-dnqp6.azure.mongodb.net/test?retryWrites=true")
